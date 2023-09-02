21182bad34b449da896a963723b87ade60544f15
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-02 14:38:26 +0300

    Added Constants and IdentityModels

---
+++ EducationPortalEL/Constants/ConstantDatas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPortalEL.Constants
+{
+    public class ConstantDatas
+    {
+        public static string STUDENTROLE { get; set; } = "Student";
+        public static string ADMNROLE { get; set; } = "Admin";
+
+    }
+}
