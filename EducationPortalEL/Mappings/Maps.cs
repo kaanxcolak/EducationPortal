b7988770bcd50ebe4204b95626a471f38ac89bc5
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-03 16:38:51 +0300

    Added Mappings

---
+++ EducationPortalEL/Mappings/Maps.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using EducationPortalEL.Models;
+using EducationPortalEL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPortalEL.Maps
+{
+    public class Maps:Profile
+    {
+        public Maps()
+        {
+            //AutoMapper ile modellerimiz ile ViewModellerimizi birleştirdik
+            CreateMap<Category, CategoryVM>().ReverseMap();
+            CreateMap<EducationalContent, EducationalContentVM>().ReverseMap();
+            CreateMap<EducationInfo, EducationInfoVM>().ReverseMap();
+            CreateMap<EducationRequest, EducationRequestVM>().ReverseMap();
+            CreateMap<Student, StudentVM>().ReverseMap();
+            CreateMap<TrainerInfo, TrainerInfoVM>().ReverseMap();
+           
+        }
+    }
+}
